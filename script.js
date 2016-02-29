af264a64d7eec5e16b4ec5821dca8392393a5cc6
Fleeck <zoorned@gmail.com>
2016-02-29 19:36:52 +0200

    moved code into functions.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var video = $('#media-video').get(0);
   var currTime = $('#current-time').get(0);
   var isToggled = false;
 
-  $('#media-video, #play-pause, #video-icon').click(function() {
+  $('#media-video, #play-pause, #video-icon').click(clickPlayPause);
+  $('#mute').click(clickMuteButton);
+  $('#full-screen').click(toggleFullscreen);
+  $('#volume-bar').change(handleVolumeBar);
+  $('#toggle-duration').click(clickToggleTimeButton);
+  $('#seek-bar').change(handleSeekBar);
+  $('#media-video').bind("timeupdate", handleTimeUpdate);
+  $(video).on('ended', playToRepeat);
+
+  function clickPlayPause() {
     if (video.paused) {
       video.play();
       $('#play-pause, #video-icon').removeClass('fa-play');
@@ -15,33 +24,21 @@ $(document).ready(function() {
       $('#play-pause').addClass('fa-play');
       $('#video-icon').css('display', 'initial');
     }
-  });
+  }
 
-  $('#mute').click(function() {
-    if (!video.muted) {
-      $('#media-video').prop('muted', true);
-      $(this).toggleClass('fa-volume-up');
-    } else {
-      $('#media-video').prop('muted', false);
-      $(this).toggleClass('fa-volume-up');
-    }
-  });
+  function handleTimeUpdate() {
+      $('#video-duration').text(((video.duration).toFixed()).toMMSS());
+      $('#current-time').text((video.currentTime.toFixed()).toMMSS());
+      $('#left-duration').text('-' + ((video.duration - video.currentTime).toFixed()).toMMSS());
+      $('#seek-bar').val(video.currentTime * 100 / video.duration);
+  }
 
-  $('#full-screen').click(function() {
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-    } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen(); // Firefox
-    } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen(); // Chrome and Safari
-    }
-  });
+  function handleSeekBar() {
+    var time = video.duration * this.value / 100;
+    video.currentTime = time;
+  }
 
-  $('#volume-bar').change(function() {
-    video.volume = $(this).get(0).value;
-  });
-
-  $('#toggle-duration').click(function() {
+  function clickToggleTimeButton() {
     if (!isToggled) {
       $('#current-time').attr('id', 'left-duration');
       isToggled = true;
@@ -49,29 +46,38 @@ $(document).ready(function() {
       $('#left-duration').attr('id', 'current-time');
       isToggled = false;
     }
-  });
+  }
 
-  $('#seek-bar').change(function() {
-    var me = this;
-    var time = video.duration * this.value / 100;
-    video.currentTime = time;
-  });
+  function clickMuteButton() {
+    if (!video.muted) {
+      $('#media-video').prop('muted', true);
+      $(this).toggleClass('fa-volume-up');
+    } else {
+      $('#media-video').prop('muted', false);
+      $(this).toggleClass('fa-volume-up');
+    }
+  }
 
-  $('#media-video').bind("timeupdate", function() {
-    $('#video-duration').text(((video.duration).toFixed()).toMMSS());
-    $('#current-time').text((video.currentTime.toFixed()).toMMSS());
-    $('#left-duration').text('-' + ((video.duration - video.currentTime).toFixed()).toMMSS());
-    $('#seek-bar').val(video.currentTime * 100 / video.duration);
-  });
+  function toggleFullscreen() {
+    if (video.requestFullscreen) {
+      video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen(); // Firefox
+    } else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen(); // Chrome and Safari
+    }
+  }
 
+  function playToRepeat() {
+    $('#play-pause, #video-icon').removeClass('fa-play fa-pause');
+    $('#play-pause, #video-icon').addClass('fa-repeat');
+    $('#video-icon').css('display', 'initial');
+  }
+
+  function handleVolumeBar() {
+    video.volume = $(this).get(0).value;
+  }
 });
-
-
-function playToRepeat() {
-  $('#play-pause, #video-icon').removeClass('fa-play fa-pause');
-  $('#play-pause, #video-icon').addClass('fa-repeat');
-  $('#video-icon').css('display', 'initial');
-}
 
 String.prototype.toMMSS = function() {
   var sec_num = parseInt(this, 10);
